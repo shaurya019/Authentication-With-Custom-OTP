3c75b4f5954b6355d7fc3ebce512deff8da924cf
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-30 14:39:57 +0530

    otp controller

--- src/main/java/Auth/OtpVerify/controller/OtpController.java
+++ src/main/java/Auth/OtpVerify/controller/OtpController.java
@@ -1,4 +1,29 @@
 package Auth.OtpVerify.controller;
 
+import Auth.OtpVerify.dto.OtpRequest;
+import Auth.OtpVerify.dto.OtpValidationRequest;
+import Auth.OtpVerify.dto.Response;
+import Auth.OtpVerify.service.impl.OtpService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/otp")
+@AllArgsConstructor
 public class OtpController {
+
+    private final OtpService otpService;
+
+    @PostMapping("sendOtp")
+    public Response sendOtp(@RequestBody OtpRequest otpRequest){
+        return otpService.sendOtp(otpRequest);
+    }
+
+    @PostMapping("validateOtp")
+    public Response validateOtp(@RequestBody OtpValidationRequest otpValidationRequest){
+        return otpService.validateOtp(otpValidationRequest);
+    }
 }
