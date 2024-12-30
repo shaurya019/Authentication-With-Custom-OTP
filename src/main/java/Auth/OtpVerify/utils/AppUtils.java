e94157c22208031e112bf2437d89e34c6d99bf1d
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-30 14:21:59 +0530

    otp genrator

--- src/main/java/Auth/OtpVerify/utils/AppUtils.java
+++ src/main/java/Auth/OtpVerify/utils/AppUtils.java
@@ -1,4 +1,21 @@
 package Auth.OtpVerify.utils;
 
+import java.util.Random;
+
 public class AppUtils {
+
+    public static String generateOtp(){
+        StringBuilder otp = new StringBuilder();
+        Random random = new Random();
+        int c = 0;
+        while(c<4){
+            otp.append(random.nextInt(10));
+            c++;
+        }
+        return otp.toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println(generateOtp());
+    }
 }
