cebf671d1ae7030f97ae0c37643b7a704e42cd5f
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-30 14:07:08 +0530

    email sender

--- src/main/java/Auth/OtpVerify/service/impl/EmailService.java
+++ src/main/java/Auth/OtpVerify/service/impl/EmailService.java
@@ -1,4 +1,39 @@
 package Auth.OtpVerify.service.impl;
 
+import Auth.OtpVerify.dto.EmailDetails;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.mail.username}")
+    private String emailSender;
+
+
+    public void sendEmail(EmailDetails emailDetails){
+        try{
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(emailSender);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            log.info("Message sent to: {}", emailDetails.getRecipient());
+            log.info("Message sender: {}", emailSender);
+
+        }catch (MailException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
